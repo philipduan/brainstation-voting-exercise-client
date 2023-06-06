9af0b83b9b1e1cf47560b3e51052694740f7ac76
Philip Du <philip_du_an@hotmail.com>
2023-06-06 15:39:40 -0700

    Add basename

--- src/App.tsx
+++ src/App.tsx
@@ -2,20 +2,23 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Login from "./pages/login";
 import Candidates from "./pages/candidates";
 import PrivateRoute from "./utils/privateRoute";
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Login />,
-  },
-  {
-    path: "/candidates",
-    element: (
-      <PrivateRoute>
-        <Candidates />
-      </PrivateRoute>
-    ),
-  },
-]);
+const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <Login />,
+    },
+    {
+      path: "/candidates",
+      element: (
+        <PrivateRoute>
+          <Candidates />
+        </PrivateRoute>
+      ),
+    },
+  ],
+  { basename: "/brainstation-voting-exercise-client" }
+);
 
 function App() {
   return <RouterProvider router={router} />;
