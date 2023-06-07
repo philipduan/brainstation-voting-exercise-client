50db7cb52691ff83d12dca1c13b4a0a643dd0981
Philip Du <philip_du_an@hotmail.com>
2023-06-06 19:57:30 -0700

    Remove basename

--- src/App.tsx
+++ src/App.tsx
@@ -2,23 +2,20 @@ import { RouterProvider, createHashRouter } from "react-router-dom";
 import Login from "./pages/login";
 import Candidates from "./pages/candidates";
 import PrivateRoute from "./utils/privateRoute";
-const router = createHashRouter(
-  [
-    {
-      path: "/",
-      element: <Login />,
-    },
-    {
-      path: "/candidates",
-      element: (
-        <PrivateRoute>
-          <Candidates />
-        </PrivateRoute>
-      ),
-    },
-  ],
-  { basename: "/brainstation-voting-exercise-client" }
-);
+const router = createHashRouter([
+  {
+    path: "/",
+    element: <Login />,
+  },
+  {
+    path: "/candidates",
+    element: (
+      <PrivateRoute>
+        <Candidates />
+      </PrivateRoute>
+    ),
+  },
+]);
 
 function App() {
   return <RouterProvider router={router} />;
